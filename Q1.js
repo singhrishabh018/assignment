73b6ab482c1bbf88081a924dcb8095d0c4fc378d
singhrishabh018 <70640026+singhrishabh018@users.noreply.github.com>
2021-05-05 20:24:34 +0530

    Add files via upload

---
+++ Q1.js
@@ -0,0 +1,8 @@
+let num1 = prompt("number 1:")
+let num2 = prompt("number 2:")
+let num3 = prompt("number 3:")
+
+var array = [num1,num2,num3].sort();
+
+document.write("Maximum number is: ",num3);
+
