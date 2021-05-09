f82e08451266944069b9ec11a9004ebc98c46a2f
singhrishabh018 <70640026+singhrishabh018@users.noreply.github.com>
2021-05-09 13:15:27 +0530

    Add files via upload

---
+++ Q8.js
@@ -0,0 +1,10 @@
+for (var x=20; x<=30; x++) {
+    if (x % 2 === 0) {
+        document.write(x + " is even"); 
+        document.write("<br>");
+    }
+    else {
+        document.write(x + " is odd");
+        document.write("<br>");
+    }
+}
